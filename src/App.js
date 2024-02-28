2bd3d5f540601c53b4851e22f8d92a536a9a21b9
Prasanna2099 <prasannavenkateshpillai@gmail.com>
2024-02-28 23:41:26 +0530

    irandam commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // App.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css'; // Import the CSS file
 
 function App() {
   const [postData, setPostData] = useState([]);
@@ -9,7 +10,7 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://v1.nocodeapi.com/prasanna2099/medium/lUOrgFIevmTUKPRn');
-        setPostData(response.data.slice(0, 10)); // Get data for the first three items
+        setPostData(response.data.slice(0, 10));
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -22,20 +23,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Blog Posts</h1>
+      </header>
+      <div className="post-container">
         {postData.length > 0 ? (
           postData.map((post, index) => (
-            <div key={index}>
+            <div className="post" key={index}>
               <h2>{post.title}</h2>
               <p>Author: {post.author}</p>
               <p>Publish Date: {new Date(post.published).toLocaleDateString()}</p>
-              <div dangerouslySetInnerHTML={{ __html: post.content }} />
+              <div className="post-content" dangerouslySetInnerHTML={{ __html: post.content }} />
               {/* Render other parts of the post as needed */}
             </div>
           ))
         ) : (
           <p>Loading...</p>
         )}
-      </header>
+      </div>
     </div>
   );
 }
